4ac07018e3e78c9a8941b80f0f9162240611d7f9
nicolaslazzos <nicolaslazzos09@gmail.com>
2024-08-01 23:11:54 -0300

    feat(source): add dynamodb for local development

--- src/lambdas/test.ts
+++ src/lambdas/test.ts
@@ -4,10 +4,26 @@ import type {
   APIGatewayProxyEventV2,
   Handler,
 } from 'aws-lambda'
+import { DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb';
+
+const client = new DynamoDBClient({
+  region: 'localhost',
+  endpoint: 'http://0.0.0.0:8000',
+  credentials: {
+    accessKeyId: 'MockAccessKeyId',
+    secretAccessKey: 'MockSecretAccessKey'
+  },
+})
 
 export const handler: Handler = async (
   _event: APIGatewayProxyEventV2,
   _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
+  const command = new ListTablesCommand({});
+
+  const response = await client.send(command);
+
+  console.log(response)
+  
   return { statusCode: 200, body: JSON.stringify({ message: 'Success' }) }
 }
